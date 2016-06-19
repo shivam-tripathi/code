3e2ea40cf8a3320ea8f009b9b1bedfd305311098
shivam-tripathi <shivam.flash@gmail.com>
2016-06-19 19:42:22 +0530

    Sockets programming in C

---
+++ c/sockets/server.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <string.h>
+
+struct new_media{
+    int                a;
+    unsigned long long b;
+    #define lala float;
+};
+int main(int argc, char const *argv[]) {
+    lala a;
+     a = 10;
+    printf("%lld\n", x);
+    return 0;
+}
