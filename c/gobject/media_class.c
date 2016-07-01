81b72d8d550cfedca0f575a5091836ae2cf711df
shivam-tripathi <shivam.flash@gmail.com>
2016-07-01 12:02:05 +0530

    Add signals

--- c/gobject/media_class.c
+++ c/gobject/media_class.c
@@ -80,15 +80,33 @@ static void media_get_property(GObject *object,
     GValue *value,
     GParamSpec *pspec);
 
+enum {
+    UNPACKED,
+    THROW_OUT,
+    LAST_SIGNAL 
+};
+
+static guint media_signal[LAST_SIGNAL] = {0,0};
+
+static void media_unpacked(Media *media);
+
+static void media_throw_out(Media *media, gboolean permanent);
+
 static void media_class_init(MediaClass *klass)
 {
+    // GParamSpec structure for instance properties
     GParamSpec *inv_nr_param;
     GParamSpec *orig_package_param;
     GObjectClass *g_object_class;
 
+    // Override class' signals
+    klass->unpacked = media_unpacked;
+    klass->throw_out = media_throw_out;
+
     // Handle to create Base class Object
     g_object_class = G_OBJECT_CLASS(klass);
 
+    // Initialize GParamSpec structures for properties
     inv_nr_param = g_param_spec_uint("inventory-id", /* identifier */
         "inventory number", /* nickname */
         "number on inventory label", /* description */
@@ -102,15 +120,38 @@ static void media_class_init(MediaClass *klass)
         FALSE,
         G_PARAM_READWRITE);
 
+    // Override set_property and get_property functions of base class
     g_object_class->set_property = media_set_property;
     g_object_class->get_property = media_get_property;
 
+    // Install properties 
     g_object_class_install_property(g_object_class,
         PROP_INV_NR,
         inv_nr_param);
     g_object_class_install_property(g_object_class,
         PROP_ORIG_PACKAGE,
         orig_package_param);
+
+    // Install signals
+    media_signal[UNPACKED] = g_signal_new("unpacked", 
+        TYPE_MEDIA,
+        G_SIGNAL_RUN_LAST|G_SIGNAL_DETAILED,
+        G_STRUCT_OFFSET(MediaClass, unpacked),
+        NULL, NULL, 
+        g_cclosure_marshal_VOID__VOID,
+        G_TYPE_NONE,
+        0);
+
+    media_signal[THROW_OUT] = g_signal_new("thow_out",
+        TYPE_MEDIA, 
+        G_SIGNAL_DETAILED|G_SIGNAL_RUN_LAST,
+        G_STRUCT_OFFSET(MediaClass, thow_out),
+        NULL, NULL,
+        g_cclosure_marshal_VOID__BOOLEAN,
+        G_TYPE_NONE,
+        1,
+        G_TYPE_BOOLEAN);
+
 }
 
 static void media_set_property(GObject *object, guint prop_id, 
@@ -159,6 +200,27 @@ static void media_get_property(GObject *object, guint prop_id, GValue *value,
     }
 }
 
+static void media_unpacked(Media *media)
+{
+    if(media->orig_package)
+    {
+        g_object_set(media, "orig-package", FALSE, NULL);
+        g_print("Media unpacked\n");
+    } else {
+        g_print("Media already unpacked\n");
+    }
+}
+
+static void media_throw_out(Media *media, gboolean permanent)
+{
+    if(permanent)
+    {
+        g_print("Thrashing media\n");
+    } else {
+        g_print("Media not in dumpster quite yet\n");
+    }
+}
+
 void print_media_inv_nr(Media *object)
 {
     Media *media;
@@ -174,12 +236,15 @@ int main()
     guint inv_nr;
     gboolean orig_package;
     g_print("%d %d\n", inv_nr, orig_package);
+    // Just the object type 
     media = g_object_new(TYPE_MEDIA, NULL);
+    // Object with default arguments 
     media = g_object_new(TYPE_MEDIA, "inventory-id" , 42, "orig-package", TRUE, NULL);    
     g_object_get(media, "inventory-id", &inv_nr, "orig-package", &orig_package, NULL);
     g_print("%d %d\n", inv_nr, orig_package);
-    g_object_set(media, "inventory-id", 30000, "orig-package", TRUE, NULL);
     g_print("%d %d\n", inv_nr, orig_package);    
+    g_object_set(media, "inventory-id", 83287, "orig-package", TRUE, NULL);
+    g_print("%d***%d\n", media->inv_nr, media->orig_package);
 }
 
 
