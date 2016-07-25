d6c7dacb5888af8e35383af3118a0e77b73dff9a
Shivam Tripathi <shivam.flash@gmail.com>
2016-07-25 23:56:59 +0530

    Find the range in which number lies in a sorted array

--- c++/practice/range_in_which_number_lies.cpp
+++ c++/practice/range_in_which_number_lies.cpp
@@ -2,29 +2,17 @@
 using namespace std;
 
 int binary_search(int a[], int start, int end, int num){
-	int mid = (start+end)/2;
+	int mid, size = end+1;
 	while(start <= end){
+		mid = (start+end)/2;
 		if(a[mid] == num)
 			break;
 		else if(a[mid] < num)
 			start = mid+1;
 		else 
 			end = mid-1;
-		mid = (start+mid)/2;
 	}
-	
-	if(num < a[0])
-		return 0;
-	if(num > a[size-1])
-		return size-1;
-
-	if(a[mid] == num)
-		return mid;
-
-	if(mid >= 0 && a[mid]<num)
-		return mid;
-	if(mid < size && a[mid]>num)
-		return mid-1;
+	return mid;
 }
 
 int cmp(const void *a, const void *b){
@@ -43,11 +31,26 @@ int main(int argc, char const *argv[])
 
 	qsort(a, sizeof(a)/sizeof(a[0]), sizeof(a[0]), cmp);
 
+	cout << "Sorted array : ";
 	for(int i=0; i<size; i++)
 		cout << a[i] << " ";
 	cout << endl;
 
-	int index = binary_search(a, size, num);
+	int index = binary_search(a, 0, size-1, num);
+
+	if(a[index] > num){
+		if(index > 0)
+			index = index-1;
+		else index = 0;
+	}
+
+	if(a[index] < num){
+		if(index < size-1)
+			index += 1;
+		else index = size-1;
+	}
+
+	cout << index << endl;
 
 	return 0;
 }
